879f3969ead0f6d18d8faecdaf4512a68473eb11
pratiek-cogito <47079730+rajprateik@users.noreply.github.com>
2024-10-23 15:36:52 +0530

    fixing error

--- src/component/dividerHorzital.js
+++
@@ -1,8 +0,0 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-
-const DividerHorzital = ({trnsform}) => {
-  return <View style={{borderBottomWidth: 1}} />;
-};
-
-export default DividerHorzital;
